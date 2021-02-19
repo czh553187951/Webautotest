8c77d3c35b6f3a6007dbc002c34bb352225309e4
Administrator <Administrator@DESKTOP-I913D3N>
2021-02-19 18:37:18 +0800

    # WARNING: head commit changed in the meantime
    123123

--- src/test/java/test/czh/handle/LoginHandle.java
+++ src/test/java/test/czh/handle/LoginHandle.java
@@ -1,12 +1,16 @@
 package test.czh.handle;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
+import test.czh.page.FirstPage;
 import test.czh.page.LoginPage;
 
 public class LoginHandle {
-	
+	static Logger logger=Logger.getLogger(LoginHandle.class);
+
 	public LoginPage loginpage;
+	public FirstPage firstpage;
 	
 	
 	public LoginHandle(WebDriver driver){
@@ -14,19 +18,39 @@ public class LoginHandle {
 	}
 	
 	public void Senduser(String user) {
+		logger.info("你输入账号为"+user);
 		loginpage.GetUserElement().sendKeys(user);
 	}
 	
-	public void SendPassword(String password) {
+	public void  SendPassword(String password) {
+		logger.info("你输入密码为"+password);
+
 		loginpage.GetPassWord().sendKeys(password);
 	}
 	
-/*	public void ClickSenvenBox(){
-		loginpage.GetSenvenElement().click();
-	}
-	*/
+
+	
 	public void ClickLogin(){
+		logger.info("你开始点击登录按钮");
+
 		loginpage.GeteLoginButton().click();
+	}
+	
+	public void ClickchooseExhibition() {
+		logger.info("你选择了展会");
+
+		firstpage.GetchooseExhibitionElement().click();
+	}
+	
+	public void SendExhibitionInput(String exhibitionInput) {
+		logger.info("你输入展会为"+exhibitionInput);
+
+		firstpage.GetExhibitionInputElement().sendKeys(exhibitionInput);
+	}
+	
+	
+	public void ClickExhibitionbutton(){
+		firstpage.GetchooseExhibitionbuttonElement().click();
 	}
 	
 	public String GetUserText(){
